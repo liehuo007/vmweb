3bf00770547b4a0f0b742626fcb022f59cf2d4f3
fengyanhui <540153807@qq.com>
2021-06-15 14:42:23 +0800

    1配置添加到单独的类中2增加输出格式配置项

---
+++ src/main/java/net/vicp/fyhui/vmweb/config/ConfigParasmeter.java
@@ -0,0 +1,19 @@
+package net.vicp.fyhui.vmweb.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class ConfigParasmeter {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${appId}")
+    private String appId;
+
+    @Value("${isShowAppId}")
+    private boolean isShowAppId;
+}
--- src/main/java/net/vicp/fyhui/vmweb/controller/WebController.java
+++ src/main/java/net/vicp/fyhui/vmweb/controller/WebController.java
@@ -1,8 +1,9 @@
 package net.vicp.fyhui.vmweb.controller;
 
+import net.vicp.fyhui.vmweb.config.ConfigParasmeter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,17 +16,19 @@ public class WebController {
 
     private final static Logger logger = LogManager.getLogger();
 
-    @Value("${server.port}")
-    private String serverPort;
-
-    @Value("${appId}")
-    private String appId;
+    @Autowired
+    private ConfigParasmeter configParasmeter;
 
     @RequestMapping
     @ResponseBody
     public String index(){
         String uuid = UUID.randomUUID().toString();
-        String result = String.format("[ %s ]-[ %s ]-[ %s ]-[ %s ]",uuid,ManagementFactory.getRuntimeMXBean().getName(),appId,serverPort);
+        String result;
+        if(configParasmeter.isShowAppId()){
+            result = String.format("[ %s ]-[ %s ]-[ %s ]-[ %s ]",uuid,ManagementFactory.getRuntimeMXBean().getName(),configParasmeter.getAppId(),configParasmeter.getServerPort());
+        } else {
+            result = String.format("[ %s ]-[ %s ]",uuid,ManagementFactory.getRuntimeMXBean().getName());
+        }
         logger.info(result);
         return result;
     }
